99e0ef3a812331d8054b84efb71bb9606df80a3b
Matheus <matheus@pop-os.localdomain>
2022-03-16 00:58:33 -0300

    first commit

---
+++ src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Usuario } from './models/usuario';
+
+@Component({
+  selector: 'app-cadastro',
+  templateUrl: './cadastro.component.html',
+  styles: []
+})
+export class CadastroComponent implements OnInit {
+
+  cadastroForm: FormGroup;
+  usuario: Usuario;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.cadastroForm = this.fb.group({
+      nome: [''],
+      cpf: [''],
+      email: [''],
+      senha: [''],
+      senhaConfirmacao: ['']
+    });
+  }
+
+  adicionarUsuario(){
+    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
+  }
+
+}
---
+++ src/app/demos/reactiveForms/cadastro/models/usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    id: string;
+    nome: string;
+    CPF: string;
+    email: string;
+    senhaConfirmaca: string;
+}
